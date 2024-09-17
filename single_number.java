53b60840027651afb27b53b499071f97ce24b0d7
shruti <122924152+shruti-ugale-code@users.noreply.github.com>
2024-09-17 20:09:05 +0530

    Update single_number.java

--- single_number.java
+++ single_number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
         for (int num : nums) {
-            ans ^= num;  
+            ans ^ = num;  
         }
         return ans;
     }
